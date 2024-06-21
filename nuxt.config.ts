19f89c04df3bdf1a4159d723d316e2192edb72dc
SongWoochan <liro9803@gmail.com>
2024-06-21 14:21:50 +0900

    메타 추가

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,6 +4,11 @@ import vuetify, { transformAssetUrls } from 'vite-plugin-vuetify'
 export default defineNuxtConfig({
   app: {
     head: {
+      title: '다정농원 대극천 복숭아',
+      link: [{ rel: 'icon', type: 'image/png', href: "/img/icons8-peach-32.png" }],
+      meta: [
+        { property:'og:image', content: '/img/pitch_1.jpg'},
+      ],
       script : [
         // 다음 주소 검색 API
         { src: '//t1.daumcdn.net/mapjsapi/bundle/postcode/prod/postcode.v2.js' },
